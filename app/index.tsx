bb4fdd6f74334dfc5a35e0275f4ed37f43127d9e
gvnzhou <h9zhou26@gmail.com>
2025-06-12 23:26:23 +0800

    feat: update index ui & app icon、name

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Button, Linking, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>🎥 如何开始录屏</Text>
+      <Text style={styles.text}>
+        1. 打开控制中心 {'\n'}
+        2. 长按录屏按钮 {'\n'}
+        3. 选择本 App {'\n'}
+        4. 点击开始录屏
+      </Text>
+      <Button title="打开设置 ➜ 控制中心" onPress={() => {
+        Linking.openURL('App-prefs:root=ControlCenter');
+      }} />
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  text: {
+    marginVertical: 10,
+  }
+});
